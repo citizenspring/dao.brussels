3d64ce59f61a7b84df2944f756903655d0e3f0cf
Xavier Damman <xdamman@gmail.com>
2021-06-14 21:19:21 +0200

    added redirects to drive, discord and telegram subdomains

--- next.config.js
+++ next.config.js
@@ -9,4 +9,42 @@ module.exports = {
       "lh6.googleusercontent.com",
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "drive.cryptoart.brussels",
+          },
+        ],
+        permanent: false,
+        destination:
+          "https://drive.google.com/drive/u/0/folders/1Cb7QcW9DBMGFEWm4RI7fm4yP0H9BBU95",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "discord.cryptoart.brussels",
+          },
+        ],
+        permanent: false,
+        destination: "https://discord.gg/aj6z88CRaK",
+      },
+      {
+        source: "/(.*)",
+        has: [
+          {
+            type: "host",
+            value: "telegram.cryptoart.brussels",
+          },
+        ],
+        permanent: false,
+        destination: "https://t.me/joinchat/y2Ybneonmn4zZmVh",
+      },
+    ];
+  },
 };
